be59d3072450c12e351ee85ab2711a9a051239db
unknown <omkarkajale65@gmail.com>
2023-12-15 22:19:12 +0530

    Python Programs

---
+++ firstPro.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+print("hello")
+
+
+# In[2]:
+
+
+name=input("Enter your name")
+print("hi",name)
+
+
+# print("hi")
+
+# In[ ]:
+
+
+print()
+
+
+# In[ ]:
+
+
+
+
